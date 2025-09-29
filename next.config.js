4250c100d7622b0a06dcb2cee878b73a8a3ff4c9
Sanat Karkhanis <139959083+sanatK24@users.noreply.github.com>
2025-09-29 21:08:52 +0530

    V4

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ const nextConfig = require('./next.config.ts');
 // Export the config as a plain object
 module.exports = {
   ...nextConfig.default,
+  output: 'standalone',
   // Add any Vercel-specific overrides here if needed
 };
