4fd3c7c426b51f73823a05baf79267f429d57696
Sanat Karkhanis <139959083+sanatK24@users.noreply.github.com>
2025-09-29 21:23:53 +0530

    V5.1

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 // @ts-check
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -24,19 +18,9 @@ const nextConfig = {
   },
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  outputFileTracingRoot: resolve(__dirname, '../../'),
   experimental: {
     // appDir is now stable in Next.js 13+
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(jsx|tsx)$/,
-        use: [resolve(__dirname, 'src/visual-edits/component-tagger-loader.js')],
-      });
-    }
-    return config;
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
